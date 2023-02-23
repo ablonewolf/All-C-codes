02e62ea85e4af90cd3ac17de6cda9dcde572bc1e
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-02-23 11:51:38 +0600

    added code on how to find the max of an array.

---
+++ Arrays/findMax.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int Max(int a[], int size)
+{
+  int max = 0;
+
+  for (int item = 0; item < size; item++)
+  {
+    if (a[item] >= max)
+    {
+      max = a[item];
+    }
+  }
+
+  return max;
+}
+
+int main()
+{
+  cout << "Please enter the size of the array : ";
+  int size;
+  cin >> size;
+  int a[size];
+  cout << "Please enter the elements of the array : " << endl;
+  for (int i = 0; i < size; i++)
+  {
+    cin >> a[i];
+  }
+  cout << "The max of the array is : " << Max(a, size) << endl;
+}
