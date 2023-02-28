b0023b56615c0bc0a278f225482a757de26f0b5b
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-02-28 16:48:06 +0600

    demonstrated string append funciton and capacity function.

---
+++ Strings/StringAdvancedFunctions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string str = "Hello ", name;
+  cout << "Please enter your name : ";
+  cin >> name;
+  cout << endl;
+  cout << str.append(name) << "." << endl;
+
+  // printing the current capacity of the string along with its length.
+  cout << "Capacity of this string : " << str.capacity() << " and its length : " << str.length() << endl;
+}
