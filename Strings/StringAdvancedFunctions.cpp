4f23951d6080014deca242d82b46e1ab27f53a7e
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-03-01 02:54:10 +0600

    demonstrated string insert function.

--- Strings/StringAdvancedFunctions.cpp
+++ Strings/StringAdvancedFunctions.cpp
@@ -6,10 +6,13 @@ int main()
 {
   string str = "Hello ", name;
   cout << "Please enter your name : ";
-  cin >> name;
+  getline(cin, name);
   cout << endl;
   cout << str.append(name) << "." << endl;
 
   // printing the current capacity of the string along with its length.
   cout << "Capacity of this string : " << str.capacity() << " and its length : " << str.length() << endl;
+  string message = "How are you, ?";
+  message.insert(13, name, 0, 4);
+  cout << message << endl;
 }
