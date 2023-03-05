4d6083e6e991ad69cd96fea180a9b9689f99772b
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-03-06 00:05:11 +0600

    made the properties of rectangle private and added getter and setters

--- OOP/Rectangle.cpp
+++ OOP/Rectangle.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 class Rectangle
 {
-public:
+private:
   int length;
   int width;
+
+public:
+  void setLength(int length)
+  {
+    this->length = length;
+  }
+
+  void setWidth(int width)
+  {
+    this->width = width;
+  }
+
+  int getLength()
+  {
+    return this->length;
+  }
+
+  int getWidth()
+  {
+    return this->width;
+  }
 
   int getArea()
   {
@@ -23,9 +44,13 @@ int main()
   Rectangle rectangle1;
   cout << "Enter the length and width of the rectangle." << endl;
   cout << "width : ";
-  cin >> rectangle1.width;
+  int length, width;
+  cin >> width;
+  rectangle1.setWidth(width);
   cout << "length : ";
-  cin >> rectangle1.length;
+  cin >> length;
+  rectangle1.setLength(length);
+  cout << "The length and width of the above Rectangle are : Length : " << rectangle1.getLength() << ", Width : " << rectangle1.getWidth() << endl;
   cout << "The area of the above rectangle is : " << rectangle1.getArea() << endl;
   cout << "The perimeter of the above rectangle is : " << rectangle1.getPerimeter() << endl;
 }
