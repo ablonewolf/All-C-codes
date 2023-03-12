1a53ae55df138487072fa640751a7c8641abfb6c
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2023-03-12 10:48:13 +0600

    replaced the previous code with method exception.

--- Exception Handlings/DivisionByZero.cpp
+++ Exception Handlings/DivisionByZero.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int division(int param1, int param2)
+{
+  if (param2 == 0)
+    throw 1;
+  return param1 / param2;
+}
+
 int main()
 {
   int number1, number2;
@@ -12,11 +19,7 @@ int main()
 
   try
   {
-    if (number2 == 0)
-    {
-      throw 1;
-    }
-    int result = number1 / number2;
+    int result = division(number1, number2);
     cout << "The result of the division is : " << result << endl;
   }
   catch (int a)
